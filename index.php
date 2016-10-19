f308c98a24a13dfdb8ddecbfc8d14a3aa543fe94
sanatiel90 <sanatielbarros@gmail.com>
2016-10-19 00:14:33 -0300

    Bootstrap adicionado e página de pesquisa criada

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<title>Projeto GitHub</title>
+</head>
+<body>
+	<div class="container">
+		<div class="col-md-3"></div>
+		
+		<div class="col-md-6">
+			<h3>Projeto GitHub - Desenvolvido por Sanatiel Barros</h3>	
+			<br>
+			<br>
+			<form method="GET" action="pesquisa.php" class="text-center">
+				<label>Digite o nome do usuário do GitHub que deseja pesquisar</label>
+				<br><br>
+				<input type="text" name="usuario" placeholder="Digite aqui sua pesquisa" style="width:250px; height:35px">
+				<button class="btn btn-success">Pesquisar</button>
+			</form>
+
+		</div>
+		
+		<div class="col-md-3"></div>		
+	</div>
+
+</body>
+</html>
